7aac15418513c4e037232b2c49311200b04062f1
khadijaumud <kh.umud07@gmail.com>
2026-03-22 16:04:19 +0000

    fix pycodestyle

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def fizzbuzz():
     for a in range(1, 101):
-        if a % 3 == 0 and a % 5 ==0:
+        if a % 3 == 0 and a % 5 == 0:
             print("FizzBuzz", end=" ")
         elif a % 3 == 0:
             print("Fizz", end=" ")
