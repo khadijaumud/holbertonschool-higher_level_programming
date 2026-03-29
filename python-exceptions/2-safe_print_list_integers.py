e45ef809cbc2dbd81548e304838b95855b9597d8
khadijaumud <kh.umud07@gmail.com>
2026-03-29 18:46:29 +0000

    add task

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
     for i in range(x):
@@ -7,7 +8,5 @@ def safe_print_list_integers(my_list=[], x=0):
             count += 1
         except (ValueError, TypeError):
             continue
-        except IndexError:
-            break
     print()
     return count
