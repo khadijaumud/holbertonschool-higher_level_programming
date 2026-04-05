39dab41b95297235502a0d55efa74e6b115be1b8
khadijaumud <kh.umud07@gmail.com>
2026-04-05 14:53:30 +0000

    add task

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -3,15 +3,15 @@
 
 
 class Square:
+    """Defines a square."""
     def __init__(self, size=0):
-         """Defines a square."""
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
 
-        def a(self):
-            """Defines an area."""
-            a = self.__size**2
-            return a
+    def area(self):
+        """Defines an area."""
+        area = self.__size**2
+        return area
