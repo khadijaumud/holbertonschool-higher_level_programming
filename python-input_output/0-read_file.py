8e14d3039159d1a94bbf710802ba4c7a025587bf
khadijaumud <khumud07@gmail.com>
2026-04-18 13:35:48 +0000

    add task

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""Module that contains a function to read a file"""
+
+
 def read_file(filename=""):
     """Reads a text file (UTF8) and prints it to stdout."""
     with open(filename, encoding="utf-8") as f:
