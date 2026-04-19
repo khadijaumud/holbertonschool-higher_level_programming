cc4ed6599582eb0ea7f3d10de30fc8204e221bee
khadijaumud <kh.umud07@gmail.com>
2026-04-19 09:47:31 +0000

    add task

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Function appends text to a file."""
+
+
+def append_write(filename="", text=""):
+    """Appends a string to text file and returns characters added."""
+    with open(filename, "a", encoding"utf-8") as f:
+        return f.write(text)
