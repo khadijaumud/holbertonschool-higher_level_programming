07d7124f86a23469b53bc60f725bb4594d1ada12
khadijaumud <kh.umud07@gmail.com>
2026-04-19 11:14:14 +0000

    add taskm

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -2,6 +2,7 @@
 """import JSON module"""
 import json
 
+
 def save_to_json_file(my_obj, filename):
     """function writes object to atext file using JSON"""
     with open(filename, "w", encoding="utf-8") as f:
