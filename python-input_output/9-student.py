493deb0797ccde3b2dd88cd03aa2d675a2cb5b8b
khadijaumud <kh.umud07@gmail.com>
2026-04-19 15:31:37 +0000

    add task

---
+++ python-input_output/9-student.py
@@ -0,0 +1,12 @@
+class Student:
+    """class"""
+
+    def __init__(self, first_name, last_name, age):
+        """class"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """dict"""
+        return self.__dict__
